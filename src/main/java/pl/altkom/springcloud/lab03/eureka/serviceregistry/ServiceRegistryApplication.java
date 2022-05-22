cdd1ee009d3f3ffe7d67990e0e8d76759a48daad
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-05-23 00:25:59 +0200

    Init

---
+++ src/main/java/pl/altkom/springcloud/lab03/eureka/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package pl.altkom.springcloud.lab03.eureka.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/altkom/springcloud/lab03/eureka/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab03.eureka.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
